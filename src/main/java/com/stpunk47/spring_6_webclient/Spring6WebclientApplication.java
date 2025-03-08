847a0de6f91f7b44dd6e062b11ca1d019aab7683
@stpunk47 <stpunk47@gmail.com>
2025-03-08 13:16:43 +0200

    Initial commit

---
+++ src/main/java/com/stpunk47/spring_6_webclient/Spring6WebclientApplication.java
@@ -0,0 +1,13 @@
+package com.stpunk47.spring_6_webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6WebclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6WebclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stpunk47/spring_6_webclient/Spring6WebclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stpunk47.spring_6_webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
