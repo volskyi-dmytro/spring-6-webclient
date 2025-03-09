c4a74a81993494c6432658b263cef8dde3b462de
@stpunk47 <stpunk47@gmail.com>
2025-03-09 23:26:47 +0200

    Add WebClient Filter configuration.

--- src/main/java/com/stpunk47/spring_6_webclient/config/WebClientConfig.java
+++ src/main/java/com/stpunk47/spring_6_webclient/config/WebClientConfig.java
@@ -3,20 +3,34 @@ package com.stpunk47.spring_6_webclient.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig implements WebClientCustomizer {
 
     private final String rootUrl;
+    private final ReactiveOAuth2AuthorizedClientManager clientManager;
 
-    public WebClientConfig(@Value("${webclient.rootUrl}") String rootUrl) {
+
+    public WebClientConfig(@Value("${webclient.rootUrl}") String rootUrl,
+                           ReactiveOAuth2AuthorizedClientManager clientManager) {
         this.rootUrl = rootUrl;
+        this.clientManager = clientManager;
     }
 
 
     @Override
     public void customize(WebClient.Builder webClientBuilder) {
-        webClientBuilder.baseUrl(rootUrl);
+
+        ServerOAuth2AuthorizedClientExchangeFilterFunction oauth =
+                new ServerOAuth2AuthorizedClientExchangeFilterFunction(clientManager);
+
+        oauth.setDefaultClientRegistrationId("springauth");
+
+        webClientBuilder
+                .filter(oauth)
+                .baseUrl(rootUrl);
     }
 }
